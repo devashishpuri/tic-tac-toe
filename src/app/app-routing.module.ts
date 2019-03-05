fd59fa06701b79661a3ffa7e3d5638c10cb03822
Devashish Puri <devashishpuri@gmail.com>
2019-03-05 22:43:15 +0530

    Game Board and Animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'game', pathMatch: 'full'},
+  { path: 'game', loadChildren: './game/game.module#GameModule' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
