a030f6c3e939f0ff1dff8ce6574bd9a553831e13
Devashish Puri <devashishpuri@gmail.com>
2019-03-06 22:27:11 +0530

    Path of NG

---
+++ src/app/_utilities/animation.helper.ts
@@ -0,0 +1,117 @@
+export class AnimationHelper {
+    _context: CanvasRenderingContext2D = null;
+    _texture: CanvasPattern = null;
+
+    constructor() { }
+
+    setContext(ctx: CanvasRenderingContext2D) {
+        this._context = ctx;
+    }
+
+    setTexture(imgUrl: string): Promise<void> {
+
+        if (!this._context) {
+            throw { message: 'Context is Undefined' };
+        }
+
+        const _img = document.createElement('img');
+        _img.setAttribute('style.display', 'none');
+
+        document.body.appendChild(_img);
+
+        _img.setAttribute('src', imgUrl);
+
+        return new Promise((resolve, reject) => {
+            console.log(_img);
+            _img.onload = () => {
+                this._texture = this._context.createPattern(_img, 'repeat');
+                resolve();
+            };
+        });
+
+    }
+
+    animateCircle(x: number, y: number, radius: number, speed: number = 0.25): Promise<void> {
+        if (this._texture) {
+            this._context.strokeStyle = <any>this._texture;
+        }
+
+        this._context.beginPath();
+
+        let from = -Math.PI / 4;
+        let to = (-Math.PI / 4) + speed;
+
+        return new Promise((resolve) => {
+            const animate = () => {
+                this._context.arc(x, y, radius, from, to);
+                this._context.stroke();
+
+                if (to >= 2 * Math.PI) {
+                    resolve();
+                    return;
+                } else {
+                    from = to;
+                    to += speed;
+                    requestAnimationFrame(animate);
+                }
+            };
+
+            animate();
+        });
+
+    }
+
+    async animateCross(x: number, y: number, radius: number): Promise<void> {
+        await this.animateLine({ x: x - radius, y: y - radius }, { x: x + radius, y: y + radius });
+        await this.animateLine({ x: x + radius, y: y - radius }, { x: x - radius, y: y + radius });
+        return;
+    }
+
+    animateLine(initial: Coords, final: Coords, speed: number = 5): Promise<void> {
+        if (this._texture) {
+            this._context.strokeStyle = <any>this._texture;
+        }
+
+        this._context.beginPath();
+        this._context.moveTo(initial.x, initial.y);
+
+        let x = initial.x;
+        let y = initial.y;
+
+        const isXPositive = initial.x < final.x;
+        const isYPositive = initial.y < final.y;
+
+        return new Promise((resolve) => {
+            const animate = () => {
+
+                if ((isXPositive && x < final.x) || (!isXPositive && x > final.x) ||
+                    (isYPositive && y < final.y) || (!isYPositive && y > final.y)) {
+                    // console.log('Doing Animation');
+                    if (isXPositive) {
+                        x = (x < final.x) ? x += speed : x;
+                    } else {
+                        x = (x > final.x) ? x -= speed : x;
+                    }
+
+                    if (isYPositive) {
+                        y = (y < final.y) ? y += speed : y;
+                    } else {
+                        y = (y > final.y) ? y -= speed : y;
+                    }
+
+                    this._context.lineTo(x, y);
+                    this._context.stroke();
+                    this._context.moveTo(x, y);
+
+                    requestAnimationFrame(animate);
+                } else {
+                    resolve();
+                    return;
+                }
+            };
+
+            animate();
+        });
+
+    }
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,141 +1,85 @@
-import { Component, OnInit } from '@angular/core';
-
-interface Coords {
-  x: number;
-  y: number;
-}
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { AnimationHelper } from '../_utilities/animation.helper';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements AfterViewInit {
 
-  private _img: Promise<HTMLImageElement>;
+  @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+
+  // Conf options
+  readonly width = parseInt(`${window.innerWidth * 0.8}`, 10);
+  readonly height = parseInt(`${window.innerWidth * 0.8}`, 10);
+  readonly lineWidth = 2;
+  // Assume Square Matrix
+  readonly matrixRows = 3;
+  readonly blockWidth = (this.width - (2 * (this.matrixRows - 1))) / this.matrixRows;
+
+  private readonly _animationHelper = new AnimationHelper();
 
   constructor() { }
 
-  ngOnInit() {
-    // Return Image as Promise when loaded
-    const img = <HTMLImageElement>document.getElementById('texture');
-    this._img = new Promise((resolve, reject) => {
-      img.onload = () => {
-        resolve(img);
-      };
-    });
-
+  ngAfterViewInit() {
     this._initCanvas();
   }
 
   async _initCanvas() {
 
-    const canvas = <HTMLCanvasElement>document.getElementById('canvas');
-
+    const canvas = this.canvas.nativeElement;
     const context = canvas.getContext('2d');
 
-    const width = (window.innerWidth * 0.8);
-    const height = (window.innerWidth * 0.8);
+    canvas.setAttribute('width', `${this.width}px`);
+    canvas.setAttribute('height', `${this.height}px`);
 
-    canvas.setAttribute('width', `${width}px`);
-    canvas.setAttribute('height', `${height}px`);
+    context.lineWidth = this.lineWidth;
 
-    const texture = context.createPattern(await this._img, 'repeat');
+    this._animationHelper.setContext(context);
+    await this._animationHelper.setTexture('/assets/images/chalk-texture.png');
 
-    context.fillStyle = texture;
-    context.lineWidth = 2;
-    // context.rect(0, 0, 100, 100);
-    // context.fill();
+    /**
+     * Draw Grid
+     */
+    await this._drawGrid();
 
-    await this._animateCircle(context, 50, 50, 25, texture);
-    await this._animateCircle(context, 150, 150, 25, texture);
-    await this._animateCross(context, 50, 250, 25, texture);
-    // this._animateLine(context, texture);
+
+    // await animationHelper.animateCircle(50, 50, 25);
+    // await animationHelper.animateCross(150, 150, 25);
+    // await animationHelper.animateCircle(50, 250, 25);
+
   }
 
-  private _animateCircle(context: CanvasRenderingContext2D, x: number, y: number, radius: number, texture?: any): Promise<void> {
-    if (texture) {
-      context.strokeStyle = texture;
+  private async _drawGrid() {
+    /**
+     * DRAW GRID LINES
+     * First Draw Horizontal Lines
+     * Then Vertical Lines
+     */
+    for (let i = 1; i < this.matrixRows; i++) {
+      const _xOffset = this.blockWidth * i + (this.lineWidth * (i - 1));
+      await this._animationHelper.animateLine({ x: _xOffset, y: 0 }, { x: _xOffset, y: this.height }, 20);
     }
 
-    context.beginPath();
-
-    const speed = 0.25;
-
-    let from = -Math.PI / 4;
-    let to = (-Math.PI / 4) + speed;
-
-    return new Promise((resolve, reject) => {
-      const animate = () => {
-        context.arc(x, y, radius, from, to);
-        context.stroke();
-
-        if (to >= 2 * Math.PI) {
-          resolve();
-          return;
-        } else {
-          from = to;
-          to += speed;
-          requestAnimationFrame(animate);
-        }
-      };
-
-      animate();
-    });
-
-  }
-
-  private async _animateCross(context: CanvasRenderingContext2D, x: number, y: number, radius: number, texture?: any): Promise<void> {
-    await this._animateLine(context, { x: x - radius, y: y - radius }, { x: x + radius, y: y + radius }, texture);
-    await this._animateLine(context, { x: x + radius, y: y - radius }, { x: x - radius, y: y + radius }, texture);
-    return;
-  }
-
-  private _animateLine(context: CanvasRenderingContext2D, initial: Coords, final: Coords, texture?: any): Promise<void> {
-    if (texture) {
-      context.strokeStyle = texture;
+    for (let i = 1; i < this.matrixRows; i++) {
+      const _yOffset = this.blockWidth * i + (this.lineWidth * (i - 1));
+      await this._animationHelper.animateLine({ x: 0, y: _yOffset }, { x: this.width, y: _yOffset }, 20);
     }
+  }
 
-    context.beginPath();
-    context.moveTo(initial.x, initial.y);
+  tap(event: MouseEvent) {
+    const canvas = this.canvas.nativeElement;
 
-    const speed = 5;
-    let x = initial.x;
-    let y = initial.y;
+    const canvasRangeStart: Coords = {
+      x: canvas.offsetLeft,
+      y: canvas.offsetTop
+    };
 
-    const isXPositive = initial.x < final.x;
-    const isYPositive = initial.y < final.y;
-
-    return new Promise((resolve, reject) => {
-      const animate = () => {
-        context.lineTo(x, y);
-        // context.lineWidth = 4;
-        context.stroke();
-
-        context.moveTo(x, y);
-
-        if (x < final.x || y < final.y) {
-          if (isXPositive) {
-            x = (x <= final.x) ? x += speed : x;
-          } else {
-            x = (x >= final.x) ? x -= speed : x;
-          }
-
-          if (isYPositive) {
-            y = (y <= final.y) ? y += speed : y;
-          } else {
-            y = (y >= final.y) ? y -= speed : y;
-          }
-
-          requestAnimationFrame(animate);
-        } else {
-          resolve();
-          return;
-        }
-      };
-
-      animate();
-    });
+    const canvasRangeEnd: Coords = {
+      x: canvas.offsetLeft + this.width,
+      y: canvas.offsetTop + this.height
+    };
 
   }
 
